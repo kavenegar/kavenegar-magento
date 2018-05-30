3169a98bfc4846d871988d4dad2057fc7eaa4717
Bagher Sohrabi <bgsrb88@gmail.com>
2018-05-30 12:36:40 +0430

    Update Observer.php

--- app/code/community/Kavenegar/KavenegarApi/Model/Observer.php
+++ app/code/community/Kavenegar/KavenegarApi/Model/Observer.php
@@ -24,85 +24,96 @@ class Kavenegar_kavenegarApi_Model_Observer {
                 //return;
 				}
 				else{
-                //getting last tracking number
-                $tracking = Mage::getResourceModel('sales/order_shipment_track_collection')->setOrderFilter($order)->getData();
-                if (!empty($tracking)) {
-                    $last = count($tracking)-1;
-                    $last_tracking_number = $tracking[$last]['track_number'];
-                }
-                else
-                    $last_tracking_number = ''; //if no tracking number set "no_tracking" message for {TRACKINGNUMBER} template
-                //getting order data to generate message template
-                $messageOrderData['{NAME}'] = $order->getShippingAddress()->getData('firstname');
-                $messageOrderData['{ORDERNUMBER}'] = $order->getIncrement_id();
-                $messageOrderData['{TRACKINGNUMBER}'] = $last_tracking_number;
-                $messageOrderData['{STORENAME}'] = $config->getStoreName();
-                $message     = strtr($message,$messageOrderData);
-                $receptor    = Mage::helper('kavenegarapi')->getPhoneNumber($order->getShippingAddress()->getData('telephone')); //or getBillingAddress
-                $sender=$config->getSender();
-				$apikey=$config->getApikey();
-				try {
-                    $apiClient = Mage::getModel('kavenegarapi/apiClient');
-                    $response = $apiClient->send($apikey,$sender,$receptor,$message);
-                    //@successs add comment to order
-                    $newComment = Mage::helper('kavenegarapi')->__('SMS notification sent (SMS id:').$response->entries->messageid.')' ;
-                    $history->setComment($newComment);
-                } 
-                catch(ApiException $e){
-                     Mage::throwException($e->getMessage());
-				}
-				catch(HttpException $e){
-					  Mage::throwException($e->getMessage());
-				} 
-                catch (Exception $e) {
-                    $newComment = Mage::helper('kavenegarapi')->__('SMS notification sending error:').' "'.$e->getMessage().'"';
-                    $history->setComment($newComment);
-                }
+                    //getting last tracking number
+                    $tracking = Mage::getResourceModel('sales/order_shipment_track_collection')->setOrderFilter($order)->getData();
+                    if (!empty($tracking)) {
+                        $last = count($tracking)-1;
+                        $last_tracking_number = $tracking[$last]['track_number'];
+                    }
+                    else{
+                        $last_tracking_number = ''; //if no tracking number set "no_tracking" message for {TRACKINGNUMBER} template
+                    }
+                    //getting order data to generate message template
+                    $messageOrderData['{NAME}'] = $order->getShippingAddress()->getData('firstname');
+                    $messageOrderData['{ORDERNUMBER}'] = $order->getIncrement_id();
+                    $messageOrderData['{TRACKINGNUMBER}'] = $last_tracking_number;
+                    $messageOrderData['{STORENAME}'] = $config->getStoreName();
+                    $message     = strtr($message,$messageOrderData);
+                    $receptor    = Mage::helper('kavenegarapi')->getPhoneNumber($order->getShippingAddress()->getData('telephone')); //or getBillingAddress
+                    $sender=$config->getSender();
+                    $apikey=$config->getApikey();
+                    $localid="1000_"+$order->getIncrement_id();                
+                    try {
+                        $apiClient = Mage::getModel('kavenegarapi/apiClient');
+                        $response = $apiClient->send($apikey,$sender,$receptor,$message,$localid);
+                        //@successs add comment to order
+                        $newComment = Mage::helper('kavenegarapi')->__('SMS notification sent (SMS id:').$response->entries->messageid.')' ;
+                        $history->setComment($newComment);
+                    } 
+                    // catch(ApiException $e){
+                    //      //Mage::throwException($e->getMessage());
+                    //      $newComment = Mage::helper('kavenegarapi')->__('SMS notification sending error:').' "'.$e->getMessage().'"';
+                    //      $history->setComment($newComment);
+                    // }
+                    // catch(HttpException $e){
+                    //       //Mage::throwException($e->getMessage());
+                    //       $newComment = Mage::helper('kavenegarapi')->__('SMS notification sending error:').' "'.$e->getMessage().'"';
+                    //       $history->setComment($newComment);
+                    // } 
+                    catch (Exception $e) {
+                        $newComment = Mage::helper('kavenegarapi')->__('SMS notification sending error:').' "'.$e->getMessage().'"';
+                        $history->setComment($newComment);
+                    }
 				}
 				
 				//Admin
 				if($newStatus=="pending"){	
-					$message = $config->getMessageTemplate("admin_pending"); //get template for new status (if active and exists)
-					if (!$message){  //return if no active message template
-					//return;
-						}else{
-					//getting last tracking number
-                $tracking = Mage::getResourceModel('sales/order_shipment_track_collection')->setOrderFilter($order)->getData();
-                if (!empty($tracking)) {
-                    $last = count($tracking)-1;
-                    $last_tracking_number = $tracking[$last]['track_number'];
-                }
-                else
-                    $last_tracking_number = ''; //if no tracking number set "no_tracking" message for {TRACKINGNUMBER} template
-                //getting order data to generate message template
-                $messageOrderData2['{NAME}'] = $order->getShippingAddress()->getData('firstname');
-                $messageOrderData2['{ORDERNUMBER}'] = $order->getIncrement_id();
-                $messageOrderData2['{TRACKINGNUMBER}'] = $last_tracking_number;
-                $messageOrderData2['{STORENAME}'] = $config->getStoreName();
-                $message     = strtr($message,$messageOrderData2);
-                $receptor    = $config->getAdminMobile();
-                $sender=$config->getSender();
-				$apikey=$config->getApikey();
-				$localid=$order->getIncrement_id();
-				try {
-                    $apiClient = Mage::getModel('kavenegarapi/apiClient');
-                    $response = $apiClient->send($apikey,$sender,$receptor,$message,$localid);
-                    //@successs add comment to order
-                    $newComment = Mage::helper('kavenegarapi')->__('SMS notification sent (SMS id:').$response->entries->messageid.')' ;
-                    $history->setComment($newComment);
-                } 
-                catch(ApiException $e){
-                     Mage::throwException($e->getMessage());
-				}
-				catch(HttpException $e){
-					  Mage::throwException($e->getMessage());
-				} 
-                catch (Exception $e) {
-                    $newComment = Mage::helper('kavenegarapi')->__('SMS notification sending error:').' "'.$e->getMessage().'"';
-                    $history->setComment($newComment);
-                }
-				}
-			}	
+                    $message = $config->getMessageTemplate("admin_pending"); //get template for new status (if active and exists)
+                    if (!$message){  //return if no active message template
+                    //return;
+                        }else{
+                    //getting last tracking number
+                    $tracking = Mage::getResourceModel('sales/order_shipment_track_collection')->setOrderFilter($order)->getData();
+                    if (!empty($tracking)) {
+                        $last = count($tracking)-1;
+                        $last_tracking_number = $tracking[$last]['track_number'];
+                    }
+                    else{
+                        $last_tracking_number = ''; //if no tracking number set "no_tracking" message for {TRACKINGNUMBER} template                        
+                    }
+                    //getting order data to generate message template
+                    $messageOrderData2['{NAME}'] = $order->getShippingAddress()->getData('firstname');
+                    $messageOrderData2['{ORDERNUMBER}'] = $order->getIncrement_id();
+                    $messageOrderData2['{TRACKINGNUMBER}'] = $last_tracking_number;
+                    $messageOrderData2['{STORENAME}'] = $config->getStoreName();
+                    $message     = strtr($message,$messageOrderData2);
+                    $receptor    = $config->getAdminMobile();
+                    $sender=$config->getSender();
+                    $apikey=$config->getApikey();
+                    $localid="2000_"+$order->getIncrement_id();
+                    try {
+                        $apiClient = Mage::getModel('kavenegarapi/apiClient');
+                        $response = $apiClient->send($apikey,$sender,$receptor,$message,$localid);
+                        //@successs add comment to order
+                        $newComment = Mage::helper('kavenegarapi')->__('SMS notification sent (SMS id:').$response->entries->messageid.')' ;
+                        $history->setComment($newComment);
+                    } 
+                    // catch(ApiException $e){
+                    //      //Mage::throwException($e->getMessage());
+                    //      $newComment = Mage::helper('kavenegarapi')->__('SMS notification sending error:').' "'.$e->getMessage().'"';
+                    //      $history->setComment($newComment);
+                    // }
+                    // catch(HttpException $e){
+                    //       //Mage::throwException($e->getMessage());
+                    //       $newComment = Mage::helper('kavenegarapi')->__('SMS notification sending error:').' "'.$e->getMessage().'"';
+                    //       $history->setComment($newComment);
+                    // } 
+                    catch (Exception $e) {
+                        $newComment = Mage::helper('kavenegarapi')->__('SMS notification sending error:').' "'.$e->getMessage().'"';
+                        $history->setComment($newComment);
+                    }
+                    }
+			    }	
             }
         }
     }  
